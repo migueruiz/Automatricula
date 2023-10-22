f562048f64b79fe4ca75a64c6a20cb3e0dc389ac
Mihnea Vidican <jvidican103@gmail.com>
2023-10-23 01:49:41 +0200

    Implementa clase e interfaces closes migueruiz#16 migueruiz#17

--- src/curso.ts
+++ src/curso.ts
@@ -1,3 +1,24 @@
+import { Asignatura } from "./asignatura";
+import { Dia, Hora } from "./types";
+
+/**
+ * 
+ * Interfaces AsignaturaConHorario y Grupo.
+ * Agregado.
+ * 
+ * Representan el horario de las asignaturas que se imparten en un grupo
+ * perteneciente a un curso y el propio grupo, respectivamente.
+ */
+
+interface AsignaturaConHorario extends Asignatura{
+    horario: Array<[Dia, Hora[]]>;
+}
+
+export interface Grupo{
+    nombre: string;
+    asignaturas: AsignaturaConHorario[];
+}
+
 /**
  * 
  * Clase Curso
@@ -6,3 +27,14 @@
  * Identifica cada curso disponible en el grado.
  */
 
+export class Curso{
+    private nombre: string;
+    private grupos: Grupo[];
+
+    // Un curso se puede crear sin tener grupos.
+    // Los grupos se pueden añadir posteriormente.
+    constructor(nombre: string){
+        this.nombre = nombre;
+        this.grupos = [];
+    }
+}
