be80691f8264019806bfc65571d608a9135f83a4
Mihnea Vidican <jvidican103@gmail.com>
2023-10-23 00:07:37 +0200

    Definida clase Estudiante closes migueruiz#13

--- src/estudiante.ts
+++ src/estudiante.ts
@@ -1,3 +1,5 @@
+import { Asignatura } from "./asignatura";
+
 /**
  *
  * Clase Estudiante.
@@ -6,4 +8,21 @@
  * Almacena los datos personales de cada estudiante en la universidad.
  * */
 
+export class Estudiante{
+    private nombre: string;
+    private apellidos: string;
+    private dni: string;
+    private correo: string;
+    private asignaturas_matriculadas: Asignatura[];
 
+    // Un estudiante se puede crear sin tener asignaturas matriculadas.
+    // Las asignaturas se pueden añadir posteriormente.
+    constructor(nombre: string, apellidos: string, dni: string, correo: string){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.correo = correo;
+        this.asignaturas_matriculadas = [];
+    }
+
+}
