8d3af78bcad9e8fa2108e6f45b7cd48ee069f68a
Mihnea Vidican <jvidican103@gmail.com>
2023-10-25 18:46:30 +0200

    Incorpora atributo horario closes #18

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,4 +1,5 @@
 import { Curso } from "./curso";
+import { HorarioAsignatura } from "./types";
 
 /**
  *
@@ -13,10 +14,28 @@ export class Asignatura{
     private codigo: string;
     private nombre: string;
     private curso: Curso;
+    private horario: HorarioAsignatura;
 
-    constructor(codigo: string, nombre: string, curso: Curso){
+    constructor(codigo: string, nombre: string, curso: Curso, horario: HorarioAsignatura){
         this.codigo = codigo;
         this.nombre = nombre;
         this.curso = curso;
+        this.horario = horario;
+    }
+
+    public getNombre(): string{
+        return this.nombre;
+    }
+
+    public getCurso(): Curso{
+        return this.curso;
+    }
+
+    public getCodigo(): string{
+        return this.codigo;
+    }
+
+    public getHorario(): HorarioAsignatura{
+        return this.horario;
     }
 }
