72103bb8eff2fdc3ff301fcc749f812001a4afb6
Mihnea Vidican <jvidican103@gmail.com>
2023-11-02 17:15:52 +0100

    Crea clase para horario #28

--- src/horario.ts
+++ src/horario.ts
@@ -1,24 +1,12 @@
-import { Asignatura } from "./asignatura";
-import { Estudiante } from "./estudiante";
-import { HorarioAsignatura, DiaConAsignaturas } from "./types";
-
 /**
  *
  * Clase Horario.
  * Objeto valor.
  * 
- * Representa información relacionada con la unión entre 
- * objetos de las clases Estudiante y Asignatura.
+ * Representa las asignaturas que se imparten en los días de la semana.
  * 
  * */
 
 export class Horario{
-    private estudiante: Estudiante;
-    private asignaturas: Asignatura[];
-
-    constructor(estudiante: Estudiante){
-        this.estudiante = estudiante;
-        this.asignaturas = estudiante.getAsignaturasMatriculadas();
-    }
-
 }
+
