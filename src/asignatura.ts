c0d78d3b9bca2a9d4591ff7b1d03e566c0c2df63
Mihnea Vidican <jvidican103@gmail.com>
2023-11-02 16:53:32 +0100

    Añade tipos para almacenar horario #25

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -6,3 +6,12 @@
  * Almacena información referente a cada asignatura.
  * 
  * */
+
+export type Dia = "Lunes" | "Martes" | "Miércoles" | "Jueves" | "Viernes";
+
+export type HoraMinutos = [number, number];
+
+export type InicioFin = [HoraMinutos, HoraMinutos];
+
+export type HorarioAsignatura = Array<[Dia, InicioFin[]]>;
+
