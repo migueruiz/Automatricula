786915d33a38dab29f9828cec54d4b1a00168df0
Mihnea Vidican <jvidican103@gmail.com>
2023-11-02 16:40:28 +0100

    Crea clase para asignatura #25

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,41 +1,8 @@
-import { Curso } from "./curso";
-import { HorarioAsignatura } from "./types";
-
 /**
  *
  * Clase Asignatura.
  * Entidad.
  * 
- * Almacena información referente a cada asignatura disponible en el grado.
+ * Almacena información referente a cada asignatura.
  * 
  * */
-
-export class Asignatura{
-    private codigo: string;
-    private nombre: string;
-    private curso: Curso;
-    private horario: HorarioAsignatura;
-
-    constructor(codigo: string, nombre: string, curso: Curso, horario: HorarioAsignatura){
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.curso = curso;
-        this.horario = horario;
-    }
-
-    public getNombre(): string{
-        return this.nombre;
-    }
-
-    public getCurso(): Curso{
-        return this.curso;
-    }
-
-    public getCodigo(): string{
-        return this.codigo;
-    }
-
-    public getHorario(): HorarioAsignatura{
-        return this.horario;
-    }
-}
