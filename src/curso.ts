4d1a0cc2334cbfc410792d2e6e4c96dbc3ac492f
Mihnea Vidican <jvidican103@gmail.com>
2023-11-02 17:04:19 +0100

    Crea clase para almacenar un curso #26

--- src/curso.ts
+++ src/curso.ts
@@ -1,59 +1,12 @@
-import { Asignatura } from "./asignatura";
-import { HorarioAsignatura } from "./types";
-
-/**
- * 
- * Interfaces Grupo.
- * Agregado.
- * 
- * Representa un determinado grupo perteneciente a un curso.
- */
-export interface Grupo{
-    nombre: string;
-    asignaturas: Asignatura[];
-}
-
 /**
  * 
  * Clase Curso
  * Entidad.
  * 
- * Identifica cada curso disponible en el grado.
+ * Identifica un curso académico.
  */
 
 export class Curso{
     private nombre: string;
-    private grupos: Grupo[];
 
-    // Un curso se puede crear sin tener grupos.
-    // Los grupos se pueden añadir posteriormente.
-    constructor(nombre: string){
-        this.nombre = nombre;
-        this.grupos = [];
-    }
-
-    public getNombre(): string{
-        return this.nombre;
-    }
-
-    public getGrupos(): Grupo[]{
-        return this.grupos;
-    }
-
-    public getGrupo(nombre_grupo: string): Grupo | undefined{
-        for (const grupo of this.grupos) {
-            if (grupo.nombre === nombre_grupo)
-                return grupo;
-        }
-        return undefined;
-    }
-
-    public getNombreGrupos(): string[]{
-        let nombres_grupos: string[] = [];
-        this.grupos.forEach(grupo => {
-            nombres_grupos.push(grupo.nombre);
-        });
-        return nombres_grupos;
-    }
-    
 }
