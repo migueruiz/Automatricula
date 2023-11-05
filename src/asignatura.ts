d802a17953be14759dd56d83718c855bb926943f
Mihnea Vidican <jvidican103@gmail.com>
2023-11-05 14:54:54 +0100

    Define tipos para el horario de una asignatura #29

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,3 +1,11 @@
+export type Dia = "Lunes" | "Martes" | "Miércoles" | "Jueves" | "Viernes";
+
+export type HoraMinutos = [number, number];
+
+export type InicioFin = [HoraMinutos, HoraMinutos];
+
+export type HorarioAsignatura = Array<[Dia, InicioFin[]]>;
+
 export class Asignatura{
 
     private nombre: string;
