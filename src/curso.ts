ff9a3021cfaf0e1f89b2ad92420967579113ed74
Mihnea Vidican <jvidican103@gmail.com>
2023-11-05 16:52:37 +0100

    Crea estructura para almacenar  grupos en curso #29

--- src/curso.ts
+++ src/curso.ts
@@ -1,8 +1,15 @@
 import { Asignatura } from './asignatura';
 
+interface Grupo{
+
+    // Un grupo es el conjunto de asignaturas de un mismo curso.
+    // Los grupos se diferencian entre sí por el horario de las asignaturas.
+    nombre: string;
+    asignaturas: Asignatura[];
+}
+
 export class Curso{
     
     private nombre: string;
-    private asignaturas: Asignatura[];
 
 }
