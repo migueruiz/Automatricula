406d9dce477d349c671e634290b1b5bde4e9f951
Mihnea Vidican <jvidican103@gmail.com>
2023-11-05 14:49:57 +0100

    Crea clase para albergar asignaturas #29

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,27 +1,6 @@
-import { Curso } from "./curso";
-
-/**
- *
- * Clase Asignatura.
- * Entidad.
- * 
- * Almacena información referente a cada asignatura.
- * 
- * */
-
-export type Dia = "Lunes" | "Martes" | "Miércoles" | "Jueves" | "Viernes";
-
-export type HoraMinutos = [number, number];
-
-export type InicioFin = [HoraMinutos, HoraMinutos];
-
-export type HorarioAsignatura = Array<[Dia, InicioFin[]]>;
-
 export class Asignatura{
 
-    private nombre: string;
-    private curso: Curso;
-    private horario: HorarioAsignatura;
+    
 
 }
 
