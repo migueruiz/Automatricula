e45cafca5947827fdba5f67ed8136cb36c828ca9
Mihnea Vidican <jvidican103@gmail.com>
2023-11-05 15:51:53 +0100

    Crea clase para representar estudiantes #29

--- src/estudiante.ts
+++ src/estudiante.ts
@@ -1,17 +1,5 @@
-import { Asignatura } from "./asignatura";
-
-/**
- *
- * Clase Estudiante.
- * Entidad.
- * 
- * Almacena los datos personales de cada estudiante en la universidad.
- * 
- * */
-
 export class Estudiante{
     
-    private nombre: string;
-    private asignaturasMatriculadas: Asignatura[];
+
 
 }
