5905d293b6e757ef8faee77b6103a77a4fe74901
Mihnea Vidican <jvidican103@gmail.com>
2023-11-09 01:59:45 +0100

    Crea clase para representar asignaturas #30
    Este commit introduce una nueva clase llamada Asignatura, la cual
    tiene dos atributos: nombre (tipo string) y curso (tipo Curso, haciendo
    referencia al curso al que pertenece).
    
    Asignatura es una entidad porque tiene una identidad única en el contexto
    de nuestro dominio. Una asignatura se puede diferenciar de otra por su nombre
    y/o curso.

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,18 +1,17 @@
 import { Curso } from "./curso";
 
-export type Dia = "Lunes" | "Martes" | "Miércoles" | "Jueves" | "Viernes";
-
-export type HoraMinutos = [number, number];
-
-export type InicioFin = [HoraMinutos, HoraMinutos];
-
-export type HorarioAsignatura = Array<[Dia, InicioFin[]]>;
+/**
+ * 
+ * Clase Asignatura.
+ * 
+ * Entidad.
+ */
 
 export class Asignatura{
 
     private nombre: string;
-    private horario: HorarioAsignatura;
     private curso: Curso;
+
 }
 
 
