2bbde48060f8e7ce68d7658efca3386bb662e99e
Mihnea Vidican <jvidican103@gmail.com>
2023-11-09 02:39:00 +0100

    Crea clase para representar un curso #30
    Mediante este commit se crea la clase Curso, la cual incluye un nombre
    y un conjunto de asignaturas como atributos. Se ha elegido esta estructura
    para poder crear objetos con un estado y comportamiento específicos.
    
    De esta forma, y según la #3, se pueden representar los cursos del
    grado y las asignaturas que pertenecen a ellos. Es por esto por lo que
    Curso resulta una entidad.

--- src/curso.ts
+++ src/curso.ts
@@ -1,16 +1,14 @@
 import { Asignatura } from './asignatura';
 
-interface Grupo{
-
-    // Un grupo es el conjunto de asignaturas de un mismo curso.
-    // Los grupos se diferencian entre sí por el horario de las asignaturas.
-    nombre: string;
-    asignaturas: Asignatura[];
-}
-
+/**
+ * 
+ * Clase Curso.
+ * 
+ * Entidad.
+ */
 export class Curso{
     
     private nombre: string;
-    private grupos: Grupo[];
+    private asignaturas: Asignatura[];
 
 }
