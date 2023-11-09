53aa48328ce1ab2e757845efb9c613fa0d06232c
migueruiz <mruiz812@correo.ugr.es>
2023-11-09 10:07:16 +0100

    Añado clases necesarias
    Al no tener los ficheros de clases del objetivo 2, los añado

---
+++ src/estudiante.ts
@@ -0,0 +1,16 @@
+
+
+/**
+ * 
+ * Clase Estudiante.
+ * 
+ * Entidad.
+ */
+export class Estudiante{
+    
+    private nombre: string;
+    private asignaturas_matriculadas: string[];
+    private asignaturas_solapadas: string[];
+    private asignaturas_alternativas: string[];
+
+}
