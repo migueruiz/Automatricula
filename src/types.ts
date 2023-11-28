fb7bed94947deac08876c686af444e28308685c5
migueruiz <mruiz812@correo.ugr.es>
2023-11-28 17:41:36 +0100

    Modifico el fichero src/types.ts. closes #42
    Cambio lo necesario para que me funcione el nuevo gestor de test.

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
 import { Asignatura } from './asignatura';
 
+export type Dia = "Lunes" | "Martes" | "Miércoles" | "Jueves" | "Viernes";
+export type InicioFin = [Date, Date];
+
 export type Grupo = {
     nombre: string;
-    dia: string;
-    horario: Date[][];
+    dia: Dia;
+    horario: InicioFin;
 };
 
 export type AsignaturaConGrupos = {
